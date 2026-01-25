37e7e74f68f4c01184f7b8819ee8ad7b3ce293df
Luke Zhu <lukezhu17@gmail.com>
2026-01-25 01:36:47 -0600

    Updated backend to allow for dynamic changes and to have a message when hitting the webpage

--- backend/server.ts
+++ backend/server.ts
@@ -1,6 +1,11 @@
 import express from 'express';
 
 const app = express();
-const PORT = process.env.PORT || 3003;
+const PORT = process.env.PORT || 8000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+    console.log('endpoint hit');
+});
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
