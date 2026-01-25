55bea4f929e9ef78057b2543cd4f21e9be5d3220
Luke Zhu <lukezhu17@gmail.com>
2026-01-25 02:13:44 -0600

    added system information to websocket server

--- backend/websocket.ts
+++ backend/websocket.ts
@@ -1,4 +1,5 @@
 import { WebSocketServer } from "ws";
+import si from "systeminformation";
 import {Server} from 'http';
 
 export function setupWebSocket(server: Server) {
@@ -12,5 +13,22 @@ export function setupWebSocket(server: Server) {
       });
   
       ws.send("Welcome to the server");
+
+      setInterval(async () => {
+        const data = await getSystemInformation();
+        ws.send(JSON.stringify({
+          type: "system_info",
+          data
+        }));
+      }, 1000);
     });
+}
+
+async function getSystemInformation() {
+  const [cpu, mem, osInfo] = await Promise.all([
+    si.currentLoad(),
+    si.mem(),
+    si.osInfo()
+  ]);
+  return { cpu, mem, osInfo };
 }
