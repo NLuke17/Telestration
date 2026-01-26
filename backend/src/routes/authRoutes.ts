ddb76dd18903733e5abc4118d0ec3c75927fe425
Luke Zhu <lukezhu17@gmail.com>
2026-01-26 00:49:23 -0600

    add OSM prisma

---
+++ backend/src/routes/authRoutes.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const router = express.Router();
+
+//CRUD
+router.post('/', (req, res) => {});
+
+router.get('/', (req, res) => {});
+
+router.put('/:id', (req, res) => {});
+
+router.delete('/:id', (req, res) => {});
+
+export default router;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import http from 'http';
 import { setupWebSocket } from './websocket.ts';
+import authRoutes from './routes/authRoutes.ts';
 
 const app = express();
 const server = http.createServer(app);
@@ -10,6 +11,8 @@ app.use(cors());
 app.use(express.json());
 
 setupWebSocket(server);
+
+app.use('/auth', authRoutes);
 
 const PORT = process.env.PORT || 8000;
 
