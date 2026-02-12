2fa115bb80a0328425aafd4f3b046af06ac2b321
averlbacher2 <aerlbacher2@huskers.unl.edu>
2026-02-12 13:48:40 -0600

    Created InputField component

---
+++ frontend/src/components/InputField.tsx
@@ -0,0 +1,22 @@
+interface InputFieldProps {
+    label: string;
+    type?: 'text' | 'password';
+    placeholder: string;
+    value: string;
+    onChange: (val: string) => void;
+}
+
+export default function InputField({ label, type='text', placeholder, value, onChange}: InputFieldProps) {
+    return (
+    <div className="flex flex-col gap-1">
+      <label className="text-sm font-semibold">{label}</label>
+      <input
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+        className="border-2 border-light-grey rounded-md px-3 py-2 outline-none focus:border-charcoal transition-colors"
+      />
+    </div>
+  );
+}
