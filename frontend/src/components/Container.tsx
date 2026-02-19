73b55328301b0e5504e36732e4c9bc8573e8af11
averlbacher2 <aerlbacher2@huskers.unl.edu>
2026-02-19 16:16:50 -0600

    Fixed Container component to accept full css size properties rather than forcing px/em

--- frontend/src/components/Container.tsx
+++ frontend/src/components/Container.tsx
@@ -8,7 +8,7 @@ interface ContainerProps {
 
 export default function Container({width, height, padding='4', children, className=''}: ContainerProps) {
     return (
-        <div style={{ width: `${width}px`, height: `${height}px`, padding: `${padding}em` }}
+        <div style={{ width: `${width}`, height: `${height}`, padding: `${padding}` }}
         className={`flex items-center justify-center ${className}`}>
             {children}
         </div>
