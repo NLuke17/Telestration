7d2c5faf331b7b5dc3dd23f42f43fccdbd6c26b8
averlbacher2 <aerlbacher2@huskers.unl.edu>
2026-02-19 16:13:12 -0600

    Removed unnecessary props for fake TutorialSlideshow

--- frontend/src/components/TutorialSlideshow.tsx
+++ frontend/src/components/TutorialSlideshow.tsx
@@ -1,8 +1,4 @@
-interface TutorialProps {
-
-}
-
-export default function TutorialSlideshow({}: TutorialProps) {
+export default function TutorialSlideshow() {
     return (
         <div className="h-full w-auto min-w-[200px] border-2 border-light-grey rounded-lg flex items-center text-center">
             Placeholder for Tutorial Slideshow
