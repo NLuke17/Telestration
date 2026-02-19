ed21ce2da68cbb67f5b5e6f1ff5d05ccffcc22f3
Toni S <115327416+srynoserotonin@users.noreply.github.com>
2026-02-19 17:09:33 -0600

    added lobby page to the app.tsx as /lobby, fixed container sizing, gaps/spacing, and margins

--- frontend/src/pages/LobbyPage.tsx
+++ frontend/src/pages/LobbyPage.tsx
@@ -7,20 +7,21 @@ import Button from '../components/Button';
     return (
         <div className="flex flex-col justify-center items-center h-screen bg-gray-50">
             <Container 
-                width='900' 
-                height='500' 
-                margin='0' 
+                width='900px' 
+                height='500px' 
+                padding='5em' 
                 className='gap-2 flex-col border-2 border-dark-grey rounded-lg bg-white shadow-xl p-12'
             >
                 {/* Header */}
                 <h1 className="text-heading-1 w-full text-left mb-0">Join Room</h1>
+                <div className='flex flex-row gap-12'>
                 
                 {/* Left: players */}
-                    <div className="flex flex-col gap-6 w-1/2">
+                    <div className="flex flex-col gap-6 w-auto">
                         <Container
-                            width='150'
+                            width='200px'
                             height='auto'
-                            margin='0'
+                            padding='0'
                             className='flex flex-col items-start border-2 border-light-grey rounded-lg bg-white shadow-xl p-2'>                          
                             <h2 className="text-lg font-bold text-left w-full">Players</h2>
                         </Container>
@@ -30,16 +31,16 @@ import Button from '../components/Button';
                     </div>
 
                 {/* Middle: invite and start */}
-                    <div className="flex flex-col gap-6 w-1/2">
+                    <div className="flex flex-col gap-6 w-full">
                         <h2 className="text-lg font-bold pb-2 text-center">Room code:</h2>
                         {/* Displaying the Room Code numbers */}
                         <p className="text-heading-1 font-bold text-center">
                             123456
                         </p>
                         <Button 
-                            label="Invite" 
+                            label="Share" 
                             className="w-fit py-3 mt-2 self-center" 
-                            onClick={() => console.log("Inviting...")}
+                            onClick={() => console.log("Sharing...")}
                         />
                         <Button 
                             label="Start" 
@@ -49,16 +50,17 @@ import Button from '../components/Button';
                     </div>
 
                 {/* Right: how to play */}
-                    <div className="flex flex-col justify-center items-center gap-6 w-1/2">
+                    <div className="flex flex-col justify-center items-center gap-6 w-auto">
                         <h2 className="text-lg font-bold pb-2 text-center">How to play</h2>
                         <Container
-                            width='200'
-                            height='250'
-                            margin='0'
+                            width='200px'
+                            height='250px'
+                            padding='0'
                             className='gap-2 flex-col border-2 border-light-grey rounded-lg bg-white shadow-xl p-12'>
                               <p className="text-center">Slideshow here</p>
                         </Container>
                     </div>
+                </div>
 
             </Container>
         </div>
