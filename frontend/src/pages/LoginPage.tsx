6dcf59c150e84828522ebe930b6eea8574208c60
averlbacher2 <aerlbacher2@huskers.unl.edu>
2026-02-19 16:16:50 -0600

    Fixed Container component to accept full css size properties rather than forcing px/em

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -7,7 +7,7 @@ import TutorialSlideshow from '../components/TutorialSlideshow';
 const LoginPage: React.FC = () => {
     return (
         <div className="flex flex-col justify-center items-center h-screen">
-            <Container width='900' height='500' padding='5' className='gap-8 flex-col border-2 border-dark-grey rounded-lg'>
+            <Container width='900px' height='500px' padding='5em' className='gap-8 flex-col border-2 border-dark-grey rounded-lg'>
                 <h1 className="text-heading-1 w-full text-left">Login</h1>
                 <div className="flex flex-row items-center justify-center w-full gap-8">
                     <InitialAvatar size='100' name='Firestone'/>
