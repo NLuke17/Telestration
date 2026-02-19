ebf1765b898e04a327981214e77908040ad3a31c
Toni S <115327416+srynoserotonin@users.noreply.github.com>
2026-02-18 19:29:22 -0600

    added a fixed player list element

--- frontend/src/pages/LobbyPage.tsx
+++ frontend/src/pages/LobbyPage.tsx
@@ -15,7 +15,20 @@ import Button from '../components/Button';
                 {/* Header */}
                 <h1 className="text-heading-1 w-full text-left mb-0">Join Room</h1>
                 
-                
+                {/* Left: players */}
+                    <div className="flex flex-col gap-6 w-1/2">
+                        <Container
+                            width='150'
+                            height='auto'
+                            margin='0'
+                            className='flex flex-col items-start border-2 border-light-grey rounded-lg bg-white shadow-xl p-2'>                          
+                            <h2 className="text-lg font-bold text-left w-full">Players</h2>
+                        </Container>
+                        <p>Player 1</p>
+                        <p>Player 2</p>
+                        <p>Player 3</p>
+                    </div>
+
             </Container>
         </div>
     );
