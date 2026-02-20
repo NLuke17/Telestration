d35845d720110f6d34f5c26f251e11c94bbe99a1
averlbacher2 <aerlbacher2@huskers.unl.edu>
2026-02-19 20:46:33 -0600

    Added ToolSizeIndicator component and added them to page

--- frontend/src/components/ToolSizeIndicator.tsx
+++ frontend/src/components/ToolSizeIndicator.tsx
@@ -1,12 +1,36 @@
 interface ToolSizeIndicatorProps {
     toolSize: string;
     className?: string;
+    color?: string;
+    variant?: 'default' | 'active';
+    onClick?: () => void;
 }
 
-export default function ToolSizeIndicator ({ toolSize, className='' }: ToolSizeIndicatorProps) {
+export default function ToolSizeIndicator({ 
+    toolSize, 
+    className = '', 
+    color = 'white',
+    variant = 'default',
+    onClick
+}: ToolSizeIndicatorProps) {
+    const baseStyles = "w-[40px] h-[40px] flex items-center justify-center rounded-full border border-white"
+    const variantStyles = {
+        default: "bg-light-grey",
+        active: "bg-dark-grey",
+    };
     return (
-        <div className="">
-
+        <div 
+            onClick = {onClick}
+            className={`${baseStyles} ${variantStyles[variant]} ${className}`}
+        >
+            <div 
+                style={{ 
+                    width: `${toolSize}px`, 
+                    height: `${toolSize}px`, 
+                    backgroundColor: color 
+                }} 
+                className="rounded-full"
+            ></div>
         </div>
-    )
+    );
 }
--- frontend/src/pages/DrawingPage.tsx
+++ frontend/src/pages/DrawingPage.tsx
@@ -8,6 +8,8 @@ import { SlActionUndo, SlActionRedo, SlPencil } from "react-icons/sl";
 import { BsEraser } from "react-icons/bs";
 import PageCounter from '../components/PageCounter';
 import TimerDisplay from '../components/TimerDisplay';
+import ToolSizeIndicator from '../components/ToolSizeIndicator';
+import Button from '../components/Button';
 
 const styles = {
   border: '0.0625rem solid #9c9c9c',
@@ -17,18 +19,22 @@ const styles = {
 const DrawingPage: React.FC = () => {
     const canvasRef = useRef<ReactSketchCanvasRef>(null);
     const [penColor, setPenColor] = useState("#000000");
+    const [selectedSize, setSelectedSize] = useState("20")
+    const sizes = ["10", "15", "20", "25", "30"]
 
     return (
         <div className="flex flex-col justify-center items-center h-screen">
             <Container width='900px' height='500px' padding='5em' className='gap-8 border-2 border-dark-grey rounded-lg flex-col'>
                 <div className='flex w-full justify-between'>
                     <PageCounter pageNum='2' totalPages='4' className='text-heading-3'/>
+                    {/* Heading */}
                     <div className='flex flex-col text-center'>
                         <div className='text-heading-3'>Hey, it's time to draw!</div>
                         <div className='text-display-prompt'>Previous Person's Sentence</div>
                     </div> 
                     <TimerDisplay minutesLeft='00' secondsLeft='30' className='text-heading-3'/>
                 </div>
+                {/* Color buttons */}
                 <div className='flex flex-row gap-6 justify-center items-center'>
                     <div className="flex flex-col gap-4">
                         <ColorButton color='black' size='30' onClick={() => {
@@ -52,6 +58,7 @@ const DrawingPage: React.FC = () => {
                             canvasRef.current?.eraseMode(false)
                         }}/>
                     </div>
+                    {/* Canvas */}
                     <ReactSketchCanvas
                         style={styles}
                         width="600px"
@@ -60,6 +67,7 @@ const DrawingPage: React.FC = () => {
                         strokeColor={penColor}
                         ref={canvasRef}
                     />
+                    {/* Tools */}
                     <div className="flex flex-col">
                         <ToolButton icon={<SlActionUndo size={30} />} onClick={() => {
                             canvasRef.current?.undo()
@@ -76,6 +84,29 @@ const DrawingPage: React.FC = () => {
                     </div>
                 </div>
             </Container>
+            {/* Tool size indicators */}
+            <div 
+                style={{
+                    width: "100%",
+                    maxWidth: "900px",
+                }}
+                className='flex flex-row justify-between items-center'
+            >
+                <div className="flex flex-row gap-2 bg-mid-grey rounded-lg px-[20px] py-[15px] border border-dark-grey">
+                    {sizes.map((size) => (
+                        <ToolSizeIndicator 
+                            key={size}
+                            toolSize={size} 
+                            variant={selectedSize === size ? 'active' : 'default'}
+                            onClick={() => {
+                                setSelectedSize(size);
+                                console.log(`Tool size changed to: ${size}`);
+                            }}
+                        />
+                    ))}
+                </div>
+                <Button label='Done'/>
+            </div>
         </div>
     );
 };
