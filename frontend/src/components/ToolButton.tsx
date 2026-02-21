47a688e165afd59a06afaf0a2b76f26d1a3f74ef
averlbacher2 <aerlbacher2@huskers.unl.edu>
2026-02-21 15:18:28 -0600

    Fixed ToolButton component to have type and aria-label

--- frontend/src/components/ToolButton.tsx
+++ frontend/src/components/ToolButton.tsx
@@ -3,19 +3,27 @@ import { GiClassicalKnowledge } from "react-icons/gi";
 interface ToolButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
     icon: React.ReactNode;
     isActive?: boolean;
-    onClick: () => void;
+    onClick: React.MouseEventHandler<HTMLButtonElement>;
 }
 
-export default function ToolButton ({icon, isActive=false, onClick, type='button', className="", ...rest }: ToolButtonProps) {
+export default function ToolButton ({
+    icon, 
+    isActive=false, 
+    className='',
+    type='button',
+    onClick,
+    ...buttonProps
+}: ToolButtonProps) {
     const baseStyles = "w-[40px] aspect-square flex justify-center rounded-md"
     const activeStyles = "bg-dark-grey text-white shadow-inner scale-95"
     const defaultStyles = "bg-transparent text-slate-600 hover:bg-light-grey"
     return (
         <button 
             type={type}
-            onClick={onClick} 
+            aria-pressed={isActive}
+            {...buttonProps}
             className={`${baseStyles} ${isActive ? activeStyles : defaultStyles}`}
-            {...rest}
+            onClick={onClick} 
         >
             {icon}
         </button>
