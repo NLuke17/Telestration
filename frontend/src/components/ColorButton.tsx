9429bee49725a77d6fd3fcb8ab776dc4138be0b4
averlbacher2 <aerlbacher2@huskers.unl.edu>
2026-02-21 15:40:39 -0600

    Added accessibility aria labels to ColorButton component

--- frontend/src/components/ColorButton.tsx
+++ frontend/src/components/ColorButton.tsx
@@ -1,13 +1,13 @@
 interface ColorButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
     color: string;
     size: string;
-    onClick: () => void;
+    onClick: React.MouseEventHandler<HTMLButtonElement>;
 }
 
-export default function ColorButton ({ color, onClick, size }: ColorButtonProps) {
+export default function ColorButton ({ color, onClick, size, ...buttonProps }: ColorButtonProps) {
 
     return (
-        <button onClick={onClick}>
+        <button onClick={onClick} type="button" {...buttonProps} >
             <div style={{ width: `${size}px`, height: `${size}px`, backgroundColor: `${color}` }} className="border-dark-grey border-1"></div>
         </button>
     );
