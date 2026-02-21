007cc1aefdf5ef29abaae8d353b61021808a2ffd
averlbacher2 <aerlbacher2@huskers.unl.edu>
2026-02-21 15:37:05 -0600

    Added content to home page & routed "Start a new room" button to Lobby page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,6 +7,7 @@ import Home from './pages/home';
 import LoginPage from './pages/LoginPage';
 import DrawingPage from './pages/DrawingPage';
 import LobbyPage from './pages/LobbyPage';
+import HomePage from './pages/HomePage';
 
 const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000';
 
@@ -17,6 +18,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           {/*<Route path="/explore" element={<Explore />} /> */}
+          <Route path="/home" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/drawing" element={<DrawingPage />} />
           <Route path="/lobby" element={<LobbyPage />} />
---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import Container from '../components/Container';
+import TutorialSlideshow from '../components/TutorialSlideshow';
+import { PiNumberCircleOne, PiNumberCircleTwo } from "react-icons/pi";
+import Button from '../components/Button';
+import { useNavigate } from 'react-router-dom';
+
+
+const HomePage: React.FC = () => {
+    const navigate = useNavigate();
+    const navigateToLobby = () => {
+        navigate('/lobby');
+    }
+    return (
+        <div className="flex flex-col justify-center items-center h-screen">
+            {/* Home content container */}
+            <Container width='900px' height='500px' padding='5em' className='gap-8 flex-col border-2 border-dark-grey rounded-lg'>
+                {/* Heading */}
+                <h1 className="text-heading-1 w-full text-left">New Game</h1>
+                {/* Main content */}
+                <div className="flex flex-row items-center justify-center w-full h-full gap-8">
+                    {/* Join a room option */}
+                    <div className='flex flex-col justify-between items-center w-hug h-full text-center gap-auto'>
+                        <PiNumberCircleOne size={33}/>
+                        <Button label="Join a room"/>
+                        <div>Join a room using the code provided by whoever is hosting!</div>
+                    </div>
+                    {/* How to Play slideshow */}
+                    <TutorialSlideshow className='w-hug'/>
+                    {/* Start a new room option */}
+                    <div className='flex flex-col justify-between items-center w-hug h-full text-center gap-auto'>
+                        <PiNumberCircleTwo size={33} />
+                        <Button 
+                            label="Start a new room"
+                            onClick={navigateToLobby}
+                        />
+                        <div>Start a room of your own and invite your friends to join!</div>
+                    </div>
+                </div>
+            </Container>
+        </div>
+    );
+}
+
+export default HomePage;
