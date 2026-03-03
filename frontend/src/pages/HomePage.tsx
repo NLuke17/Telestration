1fa9b7ad37ccd8128993b81aea50d3b69cc73dfd
averlbacher2 <aerlbacher2@huskers.unl.edu>
2026-03-03 09:45:04 -0600

    Added sign-in button

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -3,20 +3,23 @@ import Container from '../components/Container';
 import TutorialSlideshow from '../components/TutorialSlideshow';
 import { PiNumberCircleOne, PiNumberCircleTwo } from "react-icons/pi";
 import Button from '../components/Button';
-import { useNavigate } from 'react-router-dom';
-
 
 const HomePage: React.FC = () => {
-    const navigate = useNavigate();
-    const navigateToLobby = () => {
-        navigate('/lobby');
-    }
     return (
         <div className="flex flex-col justify-center items-center h-screen">
             {/* Home content container */}
             <Container width='900px' height='500px' padding='4em' className='gap-8 flex-col border-2 border-dark-grey rounded-lg'>
                 {/* Heading */}
-                <h1 className="text-heading-1 w-full text-left">New Game</h1>
+                <div className="w-full flex align-left items-center justify-between">
+                    <h1 className="text-heading-1 text-left">New Game</h1>
+                    <div className='flex flex-row items-center gap-4'>
+                        <div className='text-heading-3'>or</div>
+                        <Button
+                            className='h-fit flex'
+                            label="Sign in"
+                        />
+                    </div>
+                </div>
                 {/* Main content */}
                 <div className="flex flex-row items-center justify-center w-full h-full gap-8">
                     {/* Join a room option */}
@@ -32,7 +35,6 @@ const HomePage: React.FC = () => {
                         <PiNumberCircleTwo size={33} />
                         <Button 
                             label="Start a new room"
-                            onClick={navigateToLobby}
                         />
                         <div>Start a room of your own and invite your friends to join!</div>
                     </div>
