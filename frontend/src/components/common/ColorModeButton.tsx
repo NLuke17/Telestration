2ef93ba5d2625ab5833635d900a4b6cb2d63eff4
averlbacher2 <aerlbacher2@huskers.unl.edu>
2026-04-18 12:08:12 -0500

    Set up architecture for light/dark mode changes

---
+++ frontend/src/components/common/ColorModeButton.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { useTheme } from '../../contexts/ThemeContext';
+import { FaMoon, FaSun } from 'react-icons/fa'; 
+
+interface ColorModeButtonProps {
+    className?: string;
+}
+
+const ColorModeButton: React.FC<ColorModeButtonProps> = ({ className }) => {
+    const { theme, toggleTheme } = useTheme();
+
+    return (
+        <button
+            onClick={toggleTheme}
+            className={`
+                flex items-center justify-center p-2 rounded-lg
+                border-2 border-light-mode-border dark:border-dark-mode-border
+                bg-light-mode-text-2 dark:bg-dark-mode-button-background
+                hover:scale-110 active:scale-95
+                ${className}
+            `}
+            aria-label="Toggle Color Mode"
+        >
+            {theme === 'light' ? (
+                <FaMoon className="w-5 h-5 text-light-mode-text-1" />
+            ) : (
+                <FaSun className="w-5 h-5 text-dark-mode-text-1" />
+            )}
+        </button>
+    );
+};
+
+export default ColorModeButton;
--- frontend/src/features/auth/LoginPage.tsx
+++ frontend/src/features/auth/LoginPage.tsx
@@ -4,12 +4,23 @@ import AuthForm from './components/AuthForm';
 import Container from '../../components/common/Container';
 import InitialAvatar from '../../components/common/Avatar';
 import TutorialSlideshow from './components/TutorialSlideshow';
+import { useTheme } from '../../contexts/ThemeContext';
+
+import lightBg from '../../assets/lightmode.jpg';
+import darkBg from '../../assets/darkmode.jpg';
+import ColorModeButton from '../../components/common/ColorModeButton';
 
 const LoginPage: React.FC = () => {
+    const { theme, toggleTheme } = useTheme();
+
     return (
-        <div className="flex flex-col justify-center items-center h-screen">
-            <Container width='900px' height='500px' padding='5em' className='flex items-center justify-center gap-8 flex-col border-2 border-dark-grey rounded-lg'>
-                <h1 className="text-heading-1 w-full text-left">Login</h1>
+        <div className="flex flex-col justify-center items-center h-screen"
+        style={{ backgroundImage: `url(${theme === 'dark' ? darkBg : lightBg})`}}
+        >
+            {/* Toggle Button */}
+            <ColorModeButton className="absolute top-8 right-8" />
+            <Container width='900px' height='500px' padding='5em' className='flex items-center justify-center gap-8 flex-col border-2 border-light-mode-border dark:border-dark-mode-border rounded-lg'>
+                <h1 className="text-heading-1 text-light-mode-text-1 dark:text-dark-mode-text-1 w-full text-left ">Login</h1>
                 <div className="flex flex-row items-center justify-center w-full gap-8">
                     <InitialAvatar size='100' name='Firestone'/>
                     <AuthForm mode="login" />
