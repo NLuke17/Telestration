f7743db58a4a6453607e7da5ffe78d5ac28cb0ee
averlbacher2 <aerlbacher2@huskers.unl.edu>
2026-04-18 19:33:30 -0500

    Fixed Signup Page

--- frontend/src/features/auth/SignupPage.tsx
+++ frontend/src/features/auth/SignupPage.tsx
@@ -4,19 +4,29 @@ import AuthForm from './components/AuthForm';
 import Container from '../../components/common/Container';
 import InitialAvatar from '../../components/common/Avatar';
 import TutorialSlideshow from './components/TutorialSlideshow';
+import { useTheme } from '../../contexts/ThemeContext';
+
+import lightBg from '../../assets/lightmode.jpg';
+import darkBg from '../../assets/darkmode.jpg';
+import ColorModeButton from '../../components/common/ColorModeButton';
 
 const SignupPage: React.FC = () => {
+    const { theme, toggleTheme } = useTheme();
     return (
-        <div className="flex flex-col justify-center items-center h-screen">
+        <div className="flex flex-col justify-center items-center h-screen"
+        style={{ backgroundImage: `url(${theme === 'dark' ? darkBg : lightBg})`}}
+        >
+            {/* Toggle Button */}
+            <ColorModeButton className="absolute top-8 right-8" />
             <Container width='900px' height='550px' padding='5em' className='flex items-center justify-center gap-8 flex-col border-2 border-dark-grey rounded-lg'>
-                <h1 className="text-heading-1 w-full text-left">Sign Up</h1>
+                <h1 className="text-heading-1 text-light-mode-text-1 dark:text-dark-mode-text-1 w-full text-left">Sign Up</h1>
                 <div className="flex flex-row items-center justify-center w-full gap-8">
-                    <InitialAvatar size='100' name='Firestone'/>
+                    <InitialAvatar size='100' name='Person' iconType='astronaut' />
                     <AuthForm mode="signup" />
                     <TutorialSlideshow />
                 </div>
-                <div className="text-sm text-center">
-                    Already have an account? <Link to="/login" className="text-brand-charcoal font-semibold hover:underline">Login here</Link>
+                <div className="dark:text-dark-mode-text-1 text-sm text-center">
+                    Already have an account? <Link to="/login" className="text-brand-charcoal dark:text-dark-mode-text-2 font-semibold hover:underline">Login here</Link>
                 </div>
             </Container>
         </div>
